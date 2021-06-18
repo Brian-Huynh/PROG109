a159179a35e2907ce7c0d6c93b9e4d0aa3867a87
Brian-Huynh <82295391+Brian-Huynh@users.noreply.github.com>
2021-06-17 18:39:52 -0700

    Update artifactform.js

--- artifactform.js
+++ artifactform.js
@@ -4,11 +4,14 @@ function validateForm(){
   //First Name validation
   var validFirstname = false;
   var FirstName = document.getElementById("FirstName").value;
+  var letters = /^[A-Za-z]+$/;
 
   if (FirstName === "" ) {
     document.getElementById("errorMessage1").innerHTML = "Please enter your first name.";
   } else if (FirstName.length > 20 ) {
     document.getElementById("errorMessage1").innerHTML = "Exceeded the character limit (20)";
+  } else if (letters.test(FirstName === false) ) {
+    document.getElementById("errorMessage1").innerHTML = "Letters only.";
   }
   
   else {
@@ -24,6 +27,8 @@ function validateForm(){
     document.getElementById("errorMessage2").innerHTML = "Please enter your last name.";
   } else if (LastName.length > 50 ) {
     document.getElementById("errorMessage2").innerHTML = "Exceeded the character limit (50)";
+  } else if (letters.test(LastName === false) ) {
+    document.getElementById("errorMessage2").innerHTML = "Letters only.";
   }
 
   else {
@@ -33,10 +38,15 @@ function validateForm(){
   //Email Validation
   var validEmail = false;
   var Email = document.getElementById("Email").value;
+  var atpos = Email.indexOf("@");
+  var dotpos = Email.lastIndexOf(".");
+  
 
   if (Email === "" ) {
     document.getElementById("errorMessage3").innerHTML = "Please enter your email.";
-  } 
+  } else if (atpos< 1 || dotpos<atpos+2 || dotpos+2>= userEmail.length) { 
+    document.getElementById("errorMessage3").innerHTML = "Please enter a valid email."; 
+  }
 
   else{
     document.getElementById("errorMessage3").innerHTML = "";
@@ -48,7 +58,10 @@ function validateForm(){
 
   if (Phone === "" ) {
     document.getElementById("errorMessage4").innerHTML = "Please enter your phone number.";
-  } 
+  } else if (Phone.length > 15 ) {
+    document.getElementById("errorMessage4").innerHTML = "Exceeded character limit (15)";}
+  else if (isNaN(Phone)) {
+    document.getElementById("errorMessage4").innerHTML = "Enter a valid phone number.";}
 
   else{
     document.getElementById("errorMessage4").innerHTML = "";
@@ -145,6 +158,8 @@ function validateForm(){
   else{
     document.getElementById("errorMessage11").innerHTML = "";
    validZipCode = true;}
+  
+  
   
   if (validFirstname === false || validLastname === false || validEmail === false || validPhone === false || validUsername === false || validPassword === false || validAddress === false || validCity === false || validState === false || validCountry === false || validZipCode === false) {
     return false;
